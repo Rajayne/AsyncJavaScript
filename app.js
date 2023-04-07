061c2c24464726a41597a892bbd89a792d65c699
Rajayne <ryann.j.nealey@gmail.com>
2023-04-07 16:31:14 -0700

    Create request functions to API

---
+++ app.js
@@ -0,0 +1,26 @@
+let favNumber = 4;
+let url = "http://numbersapi.com";
+
+// 1. Request json from API
+async function requestNum() {
+    let data = await axios.get(`${url}/${favNumber}?json`);
+    console.log(data)
+}
+
+// 2. Make request for more than one number
+let favNumbers = [2, 8, 12];
+async function requestNums() {
+    let data = await axios.get(`${url}/${favNumbers}?json`);
+    console.log(data)
+}
+
+// 3. Get 4 facts and list on HTML page
+async function appendFacts() {
+    let facts = await Promise.all(
+      Array.from({ length: 4 }, () => $.getJSON(`${url}/${favNumber}?json`))
+    );
+    facts.forEach(data => {
+      $('body').append(`<p>${data.text}</p>`);
+    });
+  }
+appendFacts();
