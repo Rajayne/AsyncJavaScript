e952dda0c93f764cdfcf6c2bb5952246ebf49477
Rajayne <ryann.j.nealey@gmail.com>
2023-04-11 13:37:37 -0700

    Create event listener for draw button, shuffle button, and new deck button Created document ready listener to start new deck

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* **************************NUMBER FACTS API************************** */
 let favNumber = 4;
 let url = "http://numbersapi.com";
 
@@ -17,16 +18,6 @@ async function requestNums() {
 // requestNums()
 
 // 3. Get 4 facts and list on HTML page
-// async function appendFacts() {
-//     let facts = await Promise.all(
-//       Array.from({ length: 4 }, () => $.getJSON(`${url}/${favNumber}?json`))
-//     );
-//     facts.forEach(data => {
-//       $('body').append(`<p>${data.text}</p>`);
-//     });
-//   }
-// appendFacts();
-
 async function appendFacts() {
   for (let i = 0; i < 4; i++) {
     const data = await $.getJSON(`${url}/${favNumber}?json`);
@@ -35,22 +26,48 @@ async function appendFacts() {
 }
 // appendFacts();
 
+/* **************************DECK OF CARDS API************************** */
 cardURL = "https://deckofcardsapi.com/api/deck";
 let deckId = "icb0owo2zxrh"
+cards = ""
 
+$(document).ready(() => requestDeck())
+
+$("#draw").on("click", () => requestCard(deckId));
+$("#shuffle").on("click", () => shuffleDeck(deckId));
+$("#new").on("click", () => requestDeck());
+
+// 1. Request new deck
 async function requestDeck() {
+    cards = 52
     data = axios.get(`${cardURL}/new`)
     data.then((res) => deckId = res.data.deck_id)
+    $("#remaining").html(`${cards}`)
+    $("#draw").show()
 }
 
-let card_image = ""
+// 2. Request cards from same deck
 async function requestCard(deckId) {
+    cards -= 1
     data = axios.get(`${cardURL}/${deckId}/draw`)
-    data.then((res) => console.log(
-        `Suit: ${res.data.cards[0].suit}, Value: ${res.data.cards[0].value}`))
+    data.then((res) => $('.cards').append(
+        `<img src="${res.data.cards[0].image}"></img>`))
+    $("#remaining").html(`${cards}`)
+    if (cards == 0) {
+        $("#remaining").html(`Start a new deck!`)
+        $("#draw").hide()
+    }
 }
 
+// Resets deck to newly shuffled 52 cards
 async function shuffleDeck(deckId) {
-    let data = await axios.get(`${cardURL}/${deckId}/new`)
-    data.then(() => console.log(data.data))
+    data = axios.get(`${cardURL}/${deckId}/shuffle`)
+    data.then((res) => console.log(res.data))
+    alert("Deck shuffled successfully!")
+}
+
+// Shuffles remaining cards in deck
+async function shufflePartialDeck(deckId) {
+    data = axios.get(`${cardURL}/${deckId}/shuffle/?remaining=true`)
+    data.then((res) => console.log(res.data))
 }
