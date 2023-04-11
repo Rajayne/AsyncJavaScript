56679da95f929b2e076f1165c8c7d2d4a6114530
Rajayne <ryann.j.nealey@gmail.com>
2023-04-11 12:15:55 -0700

    Request new deck of cards function Request cards from same deck function

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ async function requestNum() {
     let data = await axios.get(`${url}/${favNumber}?json`);
     console.log(data)
 }
+// requestNum()
 
 // 2. Make request for more than one number
 let favNumbers = [2, 8, 12];
@@ -13,6 +14,7 @@ async function requestNums() {
     let data = await axios.get(`${url}/${favNumbers}?json`);
     console.log(data)
 }
+// requestNums()
 
 // 3. Get 4 facts and list on HTML page
 async function appendFacts() {
@@ -23,4 +25,24 @@ async function appendFacts() {
       $('body').append(`<p>${data.text}</p>`);
     });
   }
-appendFacts();
+// appendFacts();
+
+cardURL = "https://deckofcardsapi.com/api/deck";
+let deckId = "icb0owo2zxrh"
+
+async function requestDeck() {
+    data = axios.get(`${cardURL}/new`)
+    data.then((res) => deckId = res.data.deck_id)
+}
+
+let card_image = ""
+async function requestCard(deckId) {
+    data = axios.get(`${cardURL}/${deckId}/draw`)
+    data.then((res) => console.log(
+        `Suit: ${res.data.cards[0].suit}, Value: ${res.data.cards[0].value}`))
+}
+
+async function shuffleDeck(deckId) {
+    let data = await axios.get(`${cardURL}/${deckId}/new`)
+    data.then(() => console.log(data.data))
+}
