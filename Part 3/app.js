590fe0d656684f5e8786ee9aa031f908ad907bc0
Rajayne <ryann.j.nealey@gmail.com>
2023-04-11 15:38:43 -0700

    Create pokemon api requests

--- Part 3/app.js
+++ Part 3/app.js
@@ -1,19 +1,96 @@
 url = "https://pokeapi.co/api/v2/pokemon"
 
-totalPokemon = 1279
-
+// 1. Single request for names and URLs of pokemon
 async function requestAllPokemon() {
-    data = axios.get(`${url}`)
-    data.then((res) => console.log(res.data.results))
-}
+    data = axios.get(`${url}?limit=1000`);
+    data.then((res) => console.log(res.data));
+};
 
 async function requestRandomPokemon() {
-    pokeId = Math.ceil(Math.random() * totalPokemon)
-    data = axios.get(`${url}/${pokeId}`)
-    data.then((res) => console.log(res.data))
+    id = Math.floor(Math.random() * totalPokemon);
+    data = axios.get(`${url}?limit=1000`);
+    data.then((res) => console.log(res.data.results[id]));
+};
+
+// 2. Request three random pokemon
+async function requestThreePokemon() {
+    let allData = await $.getJSON(`${url}/?limit=1000`);
+    let randomPokemonUrls = [];
+    for (let i = 0; i < 3; i++) {
+        let randomIdx = Math.floor(Math.random() * allData.results.length);
+        let url = allData.results.splice(randomIdx, 1)[0].url;
+        randomPokemonUrls.push(url);
+    }
+    let pokemonData = await Promise.all(
+        randomPokemonUrls.map(url => $.getJSON(url))
+    );
+    pokemonData.forEach(p => console.log(p));
 }
 
-async function requestPokemonById(id) {
-    data = axios.get(`${url}/${id}`)
-    data.then((res) => console.log(res.data))
+totalSpecies = 1008
+
+async function requestPokemonDescriptions() {
+    let allData = await $.getJSON(`${url}/?limit=1000`);
+    let randomPokemonUrls = [];
+    for (let i = 0; i < 3; i++) {
+        let randomIdx = Math.floor(Math.random() * allData.results.length);
+        let url = allData.results.splice(randomIdx, 1)[0].url;
+        randomPokemonUrls.push(url);
+    }
+    let pokemonData = await Promise.all(
+        randomPokemonUrls.map(url => $.getJSON(url))
+    );
+    let speciesData = await Promise.all(
+        pokemonData.map(p => $.getJSON(p.species.url))
+    );
+    descriptions = speciesData.map(d => {
+        let descriptionObj = d.flavor_text_entries.find(
+            entry => entry.language.name === "en"
+        );
+        return descriptionObj ?
+            descriptionObj.flavor_text :
+            "No description available.";
+    });
+    descriptions.forEach((desc, i) => {
+        console.log(`${pokemonData[i].name}: ${desc}`);
+    });
+}
+
+let $btn = $("button");
+let $pokeArea = $("#pokemon");
+
+$btn.on("click", async function() {
+    $pokeArea.empty();
+    let allData = await $.getJSON(`${url}/?limit=1000`);
+    let randomPokemonUrls = [];
+    for (let i = 0; i < 3; i++) {
+        let randomIdx = Math.floor(Math.random() * allData.results.length);
+        let url = allData.results.splice(randomIdx, 1)[0].url;
+        randomPokemonUrls.push(url);
+    }
+    let pokemonData = await Promise.all(
+        randomPokemonUrls.map(url => $.getJSON(url))
+    );
+    let speciesData = await Promise.all(
+        pokemonData.map(p => $.getJSON(p.species.url))
+    );
+    speciesData.forEach((d, i) => {
+        let descriptionObj = d.flavor_text_entries.find(function(entry) {
+            return entry.language.name === "en";
+        });
+        let description = descriptionObj ? descriptionObj.flavor_text : "";
+        let name = pokemonData[i].name;
+        let imgSrc = pokemonData[i].sprites.front_default;
+        $pokeArea.append(makePokeCard(name, imgSrc, description));
+    });
+});
+
+function makePokeCard(name, imgSrc, description) {
+    return `
+      <div class="card">
+        <h1>${name}</h1>
+        <img src=${imgSrc} />
+        <p>${description}</p>
+      </div>
+    `;
 }
