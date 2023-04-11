6c9e1b6dc1015e2c6a2477c56954e80c091f03b6
Rajayne <ryann.j.nealey@gmail.com>
2023-04-11 16:02:04 -0700

    Create pokemon class and methods

--- Part 3/app.js
+++ Part 3/app.js
@@ -94,3 +94,19 @@ function makePokeCard(name, imgSrc, description) {
       </div>
     `;
 }
+
+/* **************************REWRITE POKEMON API************************** */
+
+class Pokemon {
+    constructor(id) {
+        this.id = id
+        this.types = []
+    }
+    async getInfo() {
+        let res = await axios.get(`${url}/${this.id}`)
+        this.name = res.data.name;
+        for (let type of res.data.types) {
+            this.types.push(type.type.name)
+        }
+    }
+}
